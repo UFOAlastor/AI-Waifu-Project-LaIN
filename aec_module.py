927e32881c306accdf0933d1901c63929b5d586a
UFOAlastor <1436922125@qq.com>
2025-01-13 10:42:41 +0800

    --log="回声消除跨平台支持度较低, 需要自行编译, 搭建较为复杂, 先暂缓开发"

---
+++ aec_module.py
@@ -0,0 +1,6 @@
+# aec_module.py
+
+# undo
+
+# 跨环境下编译webRTC较复杂, 难以自动化实现, 不符合目前开箱即用的项目计划
+# python我没有找到开箱即用的AEC回声消除方案, 如果有相关信息欢迎和我交流: zanderalastor@qq.com
---
+++ setup.bat
@@ -0,0 +1,5 @@
+echo 正在拉取SenseVoiceSmall模型
+:: 确保你拥有git lfs
+git lfs install
+git clone https://huggingface.co/FunAudioLLM/SenseVoiceSmall
+echo SenseVoiceSmall模型拉取完成！
